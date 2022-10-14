3f2d105cf51535beb7f8075f30559d191a12614a
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-14 18:30:03 -0500

    Started implementing the Login page

---
+++ frontend/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        LoginPage.jsx
+ * Date:            10-14-2022
+ * Description:     This component is the login page 
+ *                  of the website 
+ */
+
+function LoginPage() {
+    return (
+        <div></div>
+    );
+}
