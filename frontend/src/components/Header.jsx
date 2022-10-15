a2857fd4c6bfcebd87542d4615d23c917dd412b4
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-15 07:51:37 -0500

    Introducing the Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        Header.jsx
+ * Date:            10-15-2022
+ * Description:     This component represent the website header.
+ *                  It is used in the Login, Registration, and
+ *                  Landing pages to display the platform logo,
+ *                  and the "Sign In"/"Sign Out" buttons
+ */
+
+function Header() {
+    return (
+        <div></div>
+    );
+}
+
+export default Header;
