0457b31ac7eb99e5cab1b36315026a546c6426fb
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-15 19:49:46 -0500

    Added SVG

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,10 +7,36 @@
  *                  Landing pages to display the platform logo,
  *                  and the "Sign In"/"Sign Out" buttons
  */
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function Header() {
+    const [top, setTop] = useState(true);
+
     return (
-        <div></div>
+        <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
+            <div className="max-w-6xl mx-auto px-5 sm:px-6">
+                <div className="flex items-center justify-between h-16 md:h-20">
+
+                    {/**Branding */}
+                    <div className="flex-shrink-0 mr-4">
+                        {/**Logo */}
+                        <Link>
+                            <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                                <defs>
+                                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
+                                        <stop stopColor="#4FD1C5" offset="0%" />
+                                        <stop stopColor="#81E6D9" offset="25.871%" />
+                                        <stop stopColor="#338CF5" offset="100%" />
+                                    </radialGradient>
+                                    <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
+                                </defs>
+                            </svg>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
