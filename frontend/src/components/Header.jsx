4cc2141b8f3aa7772777faf912e2305497ab69e1
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-15 22:39:45 -0500

    Implemented useEffect for Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,6 +13,16 @@ import React, { useState, useEffect } from 'react';
 function Header() {
     const [top, setTop] = useState(true);
 
+    //When user scrolls past 10px
+    useEffect(() => {
+        const scrollHandler = () => {
+            window.pageYOffset > 10 ? setTop(false) : setTop(true)
+        };
+
+        window.addEventListener('scroll', scrollHandler);
+        return () => window.removeEventListener('scroll', scrollHandler);
+    }, [top]);
+
     return (
         <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
             <div className="max-w-6xl mx-auto px-5 sm:px-6">
@@ -39,10 +49,10 @@ function Header() {
                     <nav className='flex flex-grow'>
                         <ul className='flex flex-grow justify-end flex-wrap items-center'>
                             <li>
-                                <a to="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</a>
+                                <a className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</a>
                             </li>
                             <li>
-                                <a to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                                <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                                     <span>Sign up</span>
                                     <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
