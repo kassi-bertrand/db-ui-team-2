c5eca7a757456c2d7585f0e6965c3c4b08222208
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-17 16:02:07 -0500

    Replaced a tags with Link tags

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@
  *                  Landing pages to display the platform logo,
  *                  and the "Sign In"/"Sign Out" buttons
  */
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 
 function Header() {
@@ -31,7 +31,7 @@ function Header() {
                     {/**Branding */}
                     <div className="flex-shrink-0 mr-4">
                         {/**Logo */}
-                        <a>
+                        <Link>
                             <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                 <defs>
                                     <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
@@ -42,22 +42,22 @@ function Header() {
                                     <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
                                 </defs>
                             </svg>
-                        </a>
+                        </Link>
                     </div>
 
                     {/**Site Navigation */}
                     <nav className='flex flex-grow'>
                         <ul className='flex flex-grow justify-end flex-wrap items-center'>
                             <li>
-                                <a className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</a>
+                                <Link className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
                             </li>
                             <li>
-                                <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                                <Link className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                                     <span>Sign up</span>
                                     <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                                     </svg>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </nav>
