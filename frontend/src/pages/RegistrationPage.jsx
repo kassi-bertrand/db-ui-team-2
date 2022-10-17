3e69095599e7f5a2f94f14bfef07cf61e679790d
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-17 16:28:58 -0500

    Begin work on Registration page

---
+++ frontend/src/pages/RegistrationPage.jsx
@@ -0,0 +1,13 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        LoginPage.jsx
+ * Date:            10-17-2022
+ * Description:     This component is the login page 
+ *                  of the website 
+ */
+
+function RegistrationPage() {
+    return (
+        <div></div>
+    );
+}
