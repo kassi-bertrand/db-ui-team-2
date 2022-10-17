d2ba6321f45b407f177cf2d548618971dc95e4f5
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-17 16:34:08 -0500

    Modified comment

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -2,8 +2,9 @@
  * Author:          Kassi Bertrand
  * filename:        LoginPage.jsx
  * Date:            10-17-2022
- * Description:     This component is the login page 
- *                  of the website 
+ * Description:     This component is the Registration page 
+ *                  of the website. New users will use it to
+ *                  create their account on the platform.
  */
 
 function RegistrationPage() {
