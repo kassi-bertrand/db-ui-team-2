e883f95fa20db1eb06ad9ac9219689720b95659a
kassi-bertrand <bertran.billy@gmail.com>
2022-10-17 23:06:54 -0500

    Applied inter font to additional form elements

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -39,8 +39,8 @@ function LoginPage() {
                                     <div className="flex flex-wrap -mx-3 mb-4">
                                         <div className="w-full px-3">
                                             <div className="flex justify-between">
-                                                <label className="block text-gray-800 text-sm font-semibold mb-1" htmlFor="password">Password</label>
-                                                <Link className="text-sm text-blue-600 hover:underline font-bold">Having trouble signing in?</Link>
+                                                <label className="block text-gray-800 text-sm font-semibold mb-1 font-inter" htmlFor="password">Password</label>
+                                                <Link className="text-sm text-blue-600 hover:underline">Having trouble signing in?</Link>
                                             </div>
                                             <input id="password" type="password" className="w-full text-gray-800 form-input" placeholder="Enter your password" required />
                                         </div>
@@ -65,7 +65,7 @@ function LoginPage() {
                                                 <svg className="w-4 h-4 fill-current text-white opacity-75 flex-shrink-0 mx-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" />
                                                 </svg>
-                                                <span className="flex-auto pl-16 pr-8 -ml-16">Continue with Google</span>
+                                                <span className="flex-auto pl-16 pr-8 -ml-16 font-inter">Continue with Google</span>
                                             </button>
                                         </div>
                                     </div>
