02700fd9b794fe195aabcfc71923f0e5c60f28f9
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-18 13:36:04 -0500

    Started working on the landing page

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        LoginPage.jsx
+ * Date:            10-17-2022
+ * Description:     This component is the Landing page
+ *                  of the website. This page is the first
+ *                  new users encounter. It presents the
+ *                  company and its services
+ */
+
+function LoginPage() {
+    return (
+        <div></div>
+    );
+}
+
+export default LoginPage;
