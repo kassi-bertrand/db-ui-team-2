71f72859f26287689d398bda578879a174461787
kassi-bertrand <bertran.billy@gmail.com>
2022-10-23 18:10:44 -0500

    Added id to section for anchor link to work

--- frontend/src/components/SolutionSection.jsx
+++ frontend/src/components/SolutionSection.jsx
@@ -9,7 +9,7 @@
 
 function SolutionSection(){
     return(
-        <section className="relative">
+        <section className="relative" id="solutionSection">
              {/* Section background */}
             <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
             <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
