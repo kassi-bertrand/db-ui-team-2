0258cbdcaccca7072b091214a993ac28176bd627
kassi-bertrand <bertran.billy@gmail.com>
2022-10-23 15:39:10 -0500

    Introducing SolutionSection component

---
+++ frontend/src/components/SolutionSection.jsx
@@ -0,0 +1,18 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        HeroSection.jsx
+ * Date:            10-23-2022
+ * Description:     This component represents the landing page solution section.
+ *                  It is the second section of the landing page. It displays
+ *                  the services offered by the platform
+ */
+
+function SolutionSection(){
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default SolutionSection;
