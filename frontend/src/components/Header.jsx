1cc276fa8f3eb57197fd1683159441b26f3c7cd3
kassi-bertrand <bertran.billy@gmail.com>
2022-10-23 17:51:47 -0500

    Removed SVG from Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,23 +28,6 @@ function Header() {
             <div className="max-w-6xl mx-auto px-5 sm:px-6">
                 <div className="flex items-center justify-between h-16 md:h-20">
 
-                    {/**Branding */}
-                    <div className="flex-shrink-0 mr-4">
-                        {/**Logo */}
-                        <Link>
-                            <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                                <defs>
-                                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
-                                        <stop stopColor="#4FD1C5" offset="0%" />
-                                        <stop stopColor="#81E6D9" offset="25.871%" />
-                                        <stop stopColor="#338CF5" offset="100%" />
-                                    </radialGradient>
-                                    <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
-                                </defs>
-                            </svg>
-                        </Link>
-                    </div>
-
                     {/**Site Navigation */}
                     <nav className='flex flex-grow'>
                         <ul className='flex flex-grow justify-end flex-wrap items-center'>
