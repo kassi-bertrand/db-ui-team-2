7140f3d9475cbca3ce82a4ec90feb9868d5e9c89
kassi-bertrand <bertran.billy@gmail.com>
2022-10-23 15:50:40 -0500

    fix: Put correct filename into comment

--- frontend/src/components/SolutionSection.jsx
+++ frontend/src/components/SolutionSection.jsx
@@ -1,6 +1,6 @@
 /**
  * Author:          Kassi Bertrand
- * filename:        HeroSection.jsx
+ * filename:        SolutionSection.jsx
  * Date:            10-23-2022
  * Description:     This component represents the landing page solution section.
  *                  It is the second section of the landing page. It displays
