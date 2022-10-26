e91fe4847db18b41d1b9ef25810bc55057dbcd3e
Rofo <88415585+rudycito23@users.noreply.github.com>
2022-10-25 20:36:39 -0500

    Created 404 error page with CSS.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import { findRenderedComponentWithType } from "react-dom/test-utils";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
+import PageNotFound, { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route exact path="/register" element={<RegistrationPage />} />
         <Route exact path="/login" element={<LoginPage />} />
         {/**TODO: Add a route for 404 page */}
+        <Route exact path="/404errorpage" element={<ErrorPage />} />
       </Routes>
     </>
   );
