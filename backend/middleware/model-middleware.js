2b2e36194a82469dee96ae2709a1415f67702179
karinashin <92281567+karinashin@users.noreply.github.com>
2022-10-26 18:34:27 -0500

    Created Food, Performer, Venue Tables
    Service providers can be a food/venue provider or a performer. Depending on the type of user, tables take in specific information for type of provider. Added get requests to fetch user information depending on name, username, zipcode, etc.

--- backend/middleware/model-middleware.js
+++ backend/middleware/model-middleware.js
@@ -1,10 +1,42 @@
-const Planner = require('../models/planners');
-const User = require('../models/users');
-const createModelsMiddleware = async(req, res, next) => {
-    req.models = {
-        planner: Planner,
-        user: User
-    }
-    next();
+// const Planner = require('../models/planners');
+// const User = require('../models/users');
+// const createModelsMiddleware = async(req, res, next) => {
+//     req.models = {
+//         planner: Planner,
+//         user: User
+//     }
+//     next();
+// }
+// module.exports = { createModelsMiddleware }
+
+const connectToDatabase = require('../models/database-helper');
+const Venue = require('../models/venue');
+const Food = require('../models/food');
+const Performer = require('../models/performer');
+
+const createModelsMiddleware = async (req, res, next) => {
+   console.log('Connecting to the database');
+   const { DBQuery, disconnect } = await connectToDatabase();
+   req.models = {
+      venue: new Venue(DBQuery, disconnect),
+   }
+   req.models = {
+      food: new Food(DBQuery, disconnect),
+   }
+   req.models = {
+      performer: new Performer(DBQuery, disconnect),
+   }
+   req.disconnect = disconnect;
+   next();
 }
-module.exports = { createModelsMiddleware }
+
+const disconnectFromDatababaseMiddleware = (req, res, next) => {
+   console.log('Disconnecting from the database');
+   req.disconnect();
+   next();
+}
+
+module.exports = {
+   createModelsMiddleware,
+   disconnectFromDatababaseMiddleware
+}
