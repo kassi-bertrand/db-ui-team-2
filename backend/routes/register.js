5837d62855ce7849be21c9c06f889f30784ecff9
katebouis <78283299+katebouis@users.noreply.github.com>
2022-10-26 09:10:35 -0500

    updating get password

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,17 +1,24 @@
 const express = require ('express');
+const UserController = require('../controllers/users');
 const router = express.Router();
 
-router.post('/', async(req, res, next) => {
+outer.post('/new', async (req, res, next) => {
     try {
         const body = req.body;
         console.log(body);
-        console.log(req.models);
-        const result = await req.models.user.createNewUser(body.email, body.password);
-        res.status(201).json(result);
-    } catch(err){
-        console.error('Failed to create new user: ', err);
-        res.status(500).json({ message: err.toString()});
+        const result = await UserController.createUser(body.username, body.first_name, body.last_name, body.email, 
+            body.phone_num, body.password);
+        if(result.size === 0){
+            console.error('Failed to create new user:', err);
+            res.status(400).json({ message: err.toString() });
+        } else {
+            res.status(201).json(result);
+        }
+    } catch (err) {
+        console.error('Failed to create new user:', err);
+        res.status(400).json({ message: err.toString() });
     }
     next();
 })
+
 module.exports = router;
