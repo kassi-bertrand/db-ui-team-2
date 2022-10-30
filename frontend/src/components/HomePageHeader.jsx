4731e43dcd7f0173da7854cc145bff493389aa1e
kassi-bertrand <bertran.billy@gmail.com>
2022-10-30 18:51:30 -0500

    Started working on the home page header

---
+++ frontend/src/components/HomePageHeader.jsx
@@ -0,0 +1,14 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        HomePage.jsx
+ * Date:            10-30-2022
+ * Description:     This component is the Home page header.
+ */
+
+function HomePageHeader(){
+    return(
+        <div></div>
+    );
+}
+
+export default HomePageHeader;
