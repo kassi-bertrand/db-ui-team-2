eccad178dc75d90662079d389409b74ed27d3177
kassi-bertrand <bertran.billy@gmail.com>
2022-10-30 18:35:36 -0500

    Started working on the home page of the website

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        HomePage.jsx
+ * Date:            10-30-2022
+ * Description:     This component is the Home page
+ *                  of the website. Authenticated users will
+ *                  will be redirected to this page.
+ */
+
+function HomePage(){
+    return(
+        <div></div>
+    );
+}
+
+export default HomePage;
