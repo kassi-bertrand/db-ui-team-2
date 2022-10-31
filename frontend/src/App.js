b2773fb533d07792a7c2756ae55fb0d0de4175f6
kassi-bertrand <bertran.billy@gmail.com>
2022-10-30 20:55:09 -0500

    Added new route to home page and Removed unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
-import PageNotFound, { ErrorPage } from "./pages/ErrorPage";
+import { ErrorPage } from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
 
-
+//TODO all components here must be passed a "User" state
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/register" element={<RegistrationPage />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/*" element={<ErrorPage />} />
+        <Route exact path="/home" element={<HomePage/>} />
       </Routes>
     </>
   );
