dd36496724609a009c8ebd584ac1e8ee725a7b92
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-31 13:32:34 -0500

    Basic layout for home page tabs

--- frontend/src/components/HomePageTabs.jsx
+++ frontend/src/components/HomePageTabs.jsx
@@ -7,7 +7,17 @@
 
 function HomePageTabs(){
     return(
-        <div></div>
+        <section>
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                
+                {/**Section content */}
+                <div className="pt-20 pb-12">
+                    <div className="text-center">
+                        Tabs come here
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
