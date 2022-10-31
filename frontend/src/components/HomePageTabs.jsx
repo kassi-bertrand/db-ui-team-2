d33a9243f098ec59921fce57ac17bd4076fb1f0a
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-31 13:07:27 -0500

    Started working on a tab component for the home page

---
+++ frontend/src/components/HomePageTabs.jsx
@@ -0,0 +1,14 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        HomePageTabs.jsx
+ * Date:            10-31-2022
+ * Description:     This component is the Home page Tabs.
+ */
+
+function HomePageBanner(){
+    return(
+        <div></div>
+    );
+}
+
+export default HomePageBanner;
