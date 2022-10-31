2573224357065ebd2fd63403a6a89383c09a19c4
kassi-bertrand <bertran.billy@gmail.com>
2022-10-30 21:36:56 -0500

    Added user state to root component App + some indications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,25 @@ import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
+import { useState } from "react";
 
-//TODO all components here must be passed a "User" state
+
 function App() {
+  
+  /**
+   * NOTE: "User" state to be passed
+   * to the page components, so user
+   * infos persist across them.
+   * 
+   * This state must set in the Login or
+   * register page, after user successfully
+   * authenticate; otherwise should remain "null".
+   * 
+   * Visitors will be redirected to login page
+   * as long as the state is "null"
+   */
+  const [user, setUser] = useState(null);
+
   return (
     <>
       <Routes>
