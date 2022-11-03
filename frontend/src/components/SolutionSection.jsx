254b1299f3df64e91842b1a5c22b4de15c91e3e5
giovabos11 <giovabos11@gmail.com>
2022-11-03 10:30:14 -0500

    Learn more scroll button fixed (position modified)

--- frontend/src/components/SolutionSection.jsx
+++ frontend/src/components/SolutionSection.jsx
@@ -9,10 +9,10 @@
 
 function SolutionSection({headerRef}){
     return(
-        <section ref={headerRef} className="relative" id="solutionSection">
+        <section className="relative" id="solutionSection">
              {/* Section background */}
             <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
-            <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
+            <div ref={headerRef} className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
 
             <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
                 <div className="pt-12 md:pb-20">
