0adbf1ba0934d914855bc4a94329011db8599e42
kassi-bertrand <bertran.billy@gmail.com>
2022-11-02 22:40:37 -0500

    finalized implementation handleRegister :)

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -7,12 +7,13 @@
  *                  create their account on the platform.
  */
 import Header from "../components/Header";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useRef, useState } from "react";
 import { addUser } from "../api/usersApi";
 
-function RegistrationPage({setUser}) {
+function RegistrationPage({user, setUser}) {
   const [disable, setDisable] = useState(true);
+  const navigate = useNavigate();
 
   //UseRefs to grab form inputs' content. For more info check: https://dev.to/sobhandash/react-forms-and-useref-hook-4p1l
   const nameInput = useRef(null);
@@ -49,10 +50,13 @@ function RegistrationPage({setUser}) {
       "email": emailInput.current.value,
       "password": passwordInput.current.value,
     }
-    //2- Send signUpInfo using api function
-    addUser(signUpInfo);
-
-    //3- On success, set the user state, Re-direct user to home page
+    
+    //2-  Send signUpInfo using api function
+    //    then set user state using api response
+    //    then redirect client to home.
+    addUser(signUpInfo)
+      .then(response => setUser(response))
+      .then(() => navigate("/home"));
   }
 
   return (
