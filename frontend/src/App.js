f4e638ac6bd5905cfc91cf79cd71516d98daf987
kassi-bertrand <bertran.billy@gmail.com>
2022-11-05 21:35:35 -0500

    Removed User state, and User state is no longer passed down as props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,33 +4,18 @@ import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
-import { useState } from "react";
 
 
 function App() {
-  
-  /**
-   * NOTE: "User" state to be passed
-   * to the page components, so user
-   * infos persist across them.
-   * 
-   * This state must set in the Login or
-   * register page, after user successfully
-   * authenticate; otherwise should remain "null".
-   * 
-   * Visitors will be redirected to login page
-   * as long as the state is "null"
-   */
-  const [user, setUser] = useState(null);
 
   return (
     <>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/register" element={<RegistrationPage user={user} setUser={setUser} />} />
-        <Route exact path="/login" element={<LoginPage user={user} setUser={setUser} />} />
+        <Route exact path="/register" element={<RegistrationPage/>} />
+        <Route exact path="/login" element={<LoginPage/>} />
         <Route exact path="/*" element={<ErrorPage />} />
-        <Route exact path="/home" element={<HomePage user={user} setUser={setUser}/>} />
+        <Route exact path="/home" element={<HomePage/>} />
       </Routes>
     </>
   );
