bc698fdb792613b23231a104e47fd4427b2fbc0c
kassi-bertrand <bertran.billy@gmail.com>
2022-11-13 09:41:42 -0600

    Added a third test data for events and left TODOs instructions

--- frontend/src/components/HomePageContent.jsx
+++ frontend/src/components/HomePageContent.jsx
@@ -11,12 +11,16 @@
 import UserFeed from "./UserFeed";
 
 function HomePageContent({activeTab}){
-    //API call to get events on user feed happens here
+    //TODO: API call to get events on user feed happens here
     const events = [
         {"organizer": "Giovanni Boscan", "phoneNumber":"1111111111", "address1": "Test Rd", "address2": "apt 3421", "city": "Dallas", "state": "TX", "zipCode": "56322", "guestCount": "24", "eventType": "birthday"},
-        {"organizer": "Rudy Lucas", "phoneNumber":"2222222222", "address1": "Test Rd", "address2": "apt 2452", "city": "Dallas", "state": "TX", "zipCode": "63256", "guestCount": "50", "eventType": "birthday"}
+        {"organizer": "Rudy Lucas", "phoneNumber":"2222222222", "address1": "304 Dancing Dove Lane", "address2": "apt 2452", "city": "New York", "state": "NY", "zipCode": "63256", "guestCount": "50", "eventType": "birthday"},
+        {"organizer": "Safwan", "phoneNumber":"3333333333", "address1": "4360 Davis Street", "address2": "apt 221", "city": "Benton", "state": "GA", "zipCode": "74325", "guestCount": "15", "eventType": "birthday"}
     ];
 
+    //TODO: API call to get events created by the user here
+
+    //TODO: API call to get services created by the user here
     const TABS = {
         'Feed': <UserFeed events={events}/>,
         'Events': <div>Event by the user</div>,
