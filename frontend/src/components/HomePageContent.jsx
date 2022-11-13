22935c82a4639c498b59ca32aabfba03429f6021
kassi-bertrand <bertran.billy@gmail.com>
2022-11-13 08:55:18 -0600

    Re-organized HomePageContent component

--- frontend/src/components/HomePageContent.jsx
+++ frontend/src/components/HomePageContent.jsx
@@ -11,28 +11,29 @@
 import UserFeed from "./UserFeed";
 
 function HomePageContent({activeTab}){
-    if(activeTab == "Feed"){
-        //API call to get events on user feed happens here
-        const events = [
-            {"organizer": "Kassi Bertrand", "phoneNumber":"1111111111", "address1": "Test Rd", "address2": "apt 3421", "city": "Dallas", "state": "TX", "zipCode": "56322", "guestCount": "24", "eventType": "birthday"},
-            {"organizer": "Rudy Lucas", "phoneNumber":"2222222222", "address1": "Test Rd", "address2": "apt 2452", "city": "Dallas", "state": "TX", "zipCode": "63256", "guestCount": "50", "eventType": "birthday"}
-        ];
-        return(
-            <section className="font-inter">
-                <div className="max-w-screen-xl mx-auto px-4 sm:px-6">
-                    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
-                        <UserFeed events={events}/>
-                    </div>
+    //API call to get events on user feed happens here
+    const events = [
+        {"organizer": "Kassi Bertrand", "phoneNumber":"1111111111", "address1": "Test Rd", "address2": "apt 3421", "city": "Dallas", "state": "TX", "zipCode": "56322", "guestCount": "24", "eventType": "birthday"},
+        {"organizer": "Rudy Lucas", "phoneNumber":"2222222222", "address1": "Test Rd", "address2": "apt 2452", "city": "Dallas", "state": "TX", "zipCode": "63256", "guestCount": "50", "eventType": "birthday"}
+    ];
+
+    const TABS = {
+        'Feed': <UserFeed events={events}/>,
+        'Events': <div>Event by the user</div>,
+        'Services': <div>Services by the users</div>
+    };
+
+    return(
+        <section className="font-inter">
+            <div className="max-w-screen-xl mx-auto px-4 sm:px-6">
+                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+                    {TABS[activeTab]}
                 </div>
-            </section>
-        );
-    }
-    else if(activeTab === "Events"){
+            </div>
+        </section>
+    );
 
-    }
-    else if(activeTab === "Services"){
 
-    }
 }
 
 export default HomePageContent;
