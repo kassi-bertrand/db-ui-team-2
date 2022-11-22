1c0714a1a5c33e77414554c62ca57a4854d7cfec
kassi-bertrand <bertran.billy@gmail.com>
2022-11-22 15:29:30 -0600

    Left TODOs for Rudy, and Giovanni

---
+++ frontend/src/components/NewPerformanceServiceForm.jsx
@@ -0,0 +1,24 @@
+/**
+ * Author:          Giovanni Boscan & Kassi Bertrand
+ * filename:        NewVenueServiceForm.jsx
+ * Date:            11-22-2022
+ * Description:     This component renders the form presented
+ *                  to the user when creating a new performance service. This
+ *                  component is rendered as a modal.
+ */
+
+function NewPerformanceServiceForm({user, services, isOpen, setIsOpen, setServices}){
+    {/**Implement this form. Inspire yourself from NewCateringServiceForm.jsx */}
+    {/**Make sure to respect the snake convention :) */}
+
+    {
+        /**
+         * The form fields are:
+         * Name
+         * phone number
+         * details
+         */
+    }
+}
+
+export default NewPerformanceServiceForm
---
+++ frontend/src/components/PerformanceServices.jsx
@@ -0,0 +1,53 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        VenueServices.jsx
+ * Date:            11-22-2022
+ * Description:     This component is used in the
+ *                  UserService component and renders
+ *                  all performance services created by the 
+ *                  user.
+ */
+
+import { useContext, useEffect } from "react";
+import { useState } from "react";
+import { MyContext } from "../pages/HomePage";
+import NewPerformanceServiceForm from "./NewPerformanceServiceForm";
+
+function PerformanceServices(){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [services, setServices] = useState([]);
+    const {user, setUser} = useContext(MyContext);
+
+    //Get all performance services created by the user and set "services" state
+    //TODO: Complete This useEffect function
+    useEffect(()=>{
+        const fetchPeformance = async() => {
+            //getPerformanceServicesById(user.id).then(response => ...);
+        }
+
+        fetchPeformance()
+    }, [user]);
+
+    return(
+        <div>
+            <div className="mt-8 mb-8">
+                <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">Performance 💃🕺</h1>
+                <p className="mt-1.5 text-sm text-gray-500">
+                    Below, the performance services you're offering
+                </p>
+            </div>
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+                {/**First, Render Plus button */}
+                <PlusButton title="New Performace" setIsOpen={setIsOpen}/>
+                {/**TODO: Then, Render all Performance cards here */}
+                {
+
+                }
+                <NewPerformanceServiceForm user={user} services={services} isOpen={isOpen} setIsOpen={setIsOpen} setServices={setServices}/>
+            </div>
+        </div>
+    );
+}
+
+export default PerformanceServices;
