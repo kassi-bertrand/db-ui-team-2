715f8866486f30951e7ce84d2e0cd150113efcf6
kassi-bertrand <bertran.billy@gmail.com>
2022-11-22 14:52:12 -0600

    Completed the "UserService" component to accomodate backend

--- frontend/src/components/UserService.jsx
+++ frontend/src/components/UserService.jsx
@@ -1,50 +1,15 @@
- import { useContext } from "react";
-import { useEffect, useState } from "react";
- import { getServicesById } from "../api/servicesApi";
-import { MyContext } from "../pages/HomePage";
- import ServiceCard from "./EventCard";
- import NewServiceForm from "./NewServiceForm";
- import PlusButton from "./PlusButton";
+import CateringServices from "./CateringServices";
+import PerformanceServices from "./PerformanceServices";
+import VenueServices from "./VenueServices";
  
- function UserService(){
-     const [isOpen, setIsOpen] = useState(false);
-     const [services, setServices] = useState([]);
-     const {user, setUser} = useContext(MyContext);
+function UserService(){
+    return(
+        <>
+            <CateringServices/>
+            <VenueServices/>
+            <PerformanceServices/>
+        </>
+    );
+}
  
-     //Get services created by the user and set "services" state
-     useEffect(()=>{
-         //getServicesById(user.id).then(response => setEvents(response));
-     }, [])
-     return(
-         <>
-            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
-                {/**First, Render Plus button */}
-                <PlusButton title="New Service" setIsOpen={setIsOpen}/>
-                {/**Then, Render all services */}
-                {
-                    services.map((aService, index) =>{
-                        return(
-                            <ServiceCard 
-                                user_id={aService.user_id}
-                                name={aService.name}
-                                phone_num={aService.phone_num}
-                                street={aService.street}
-                                city={aService.city}
-                                state={aService.state}
-                                zip_code={aService.zip_code}
-                                event_date={aService.event_date}
-                                budget={aService.event_date}
-                                guest_count={aService.guest_count}
-                                occasion={aService.occasion}
-                                details={aService.details}
-                            />
-                        );
-                    })
-                }
-                <NewServiceForm user={user} services={services} isOpen={isOpen} setIsOpen={setIsOpen} setServices={setServices}/>
-            </div>
-         </>
-     );
- }
- 
- export default UserService;
+export default UserService;
