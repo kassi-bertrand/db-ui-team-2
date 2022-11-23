661c72a72a0e1f0a2173347b28246712c965757d
kassi-bertrand <bertran.billy@gmail.com>
2022-11-23 13:11:21 -0600

    Modified Filter button color

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -27,7 +27,7 @@ export default function SearchBar(){
                     <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search"/>
                 </div>
                 <div className="mb-4 ml-2">
-                    <button className="px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button">Filter</button>
+                    <button className="px-6 py-2 border-2 border-orange-300 text-orange-400 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button">Filter</button>
                 </div>
             </div>
         </form>
